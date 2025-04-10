a0224cc695ecc0db221110e49d06fcc577a3ad23
karhel <karheltmarr@hotmail.fr>
2025-04-10 22:19:25 +0200

    OK Téléchargement PDF après clic sur bouton

---
+++ src/Controller/UserController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserFormType;
+use App\Service\PdfGenerator;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+final class UserController extends AbstractController
+{
+    #[Route('/user', name: 'app_user')]
+    public function index(UserRepository $userRepository): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'users' => $userRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/user/create', name: 'app_user_create')]
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $userFormCreate = $this->createForm(UserFormType::class, $user);
+
+        $userFormCreate->handleRequest($request);
+
+        if($userFormCreate->isSubmitted() && $userFormCreate->isValid())
+        {
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_user_create_pdf', [], 307);
+        }
+
+        return $this->render('user/create.html.twig', [
+            'userFormCreate' => $userFormCreate
+        ]);
+    }
+
+    #[Route('/user/create-pdf', name: 'app_user_create_pdf', methods: ['POST'])]
+    public function generateCreatePdf(Request $request, UserRepository $userRepository, PdfGenerator $pdfGenerator): Response
+    {
+        $password = $request->get('user_form')['password'];
+
+        // Je renvois le fichier sous la forme d'un stream
+        return $pdfGenerator->getStreamResponse("<h1>Hello World! $password</h1>", 'file.pdf');
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $username = null;
+
+    #[ORM\Column(length: 150)]
+    private ?string $lastName = null;
+
+    #[ORM\Column(length: 150)]
+    private ?string $firstName = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $password = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): static
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): static
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): static
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+}
